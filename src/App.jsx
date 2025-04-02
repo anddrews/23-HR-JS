4b57aad39d4806bcd6ca13a6acf398b9500c7741
Andrei Kandratsenka <andrei_kandratsenka@epam.com>
2025-04-02 23:26:58 +0300

    hw1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
 
 function App() {
-
+  const arr = [1, 2, 3, 4, 5];
   return (
+    <>
       <h1>Hello world</h1>
+      {arr.map((item, index) => (<div key={index}>{item}</div>))}
+    </>
   )
 }
 
