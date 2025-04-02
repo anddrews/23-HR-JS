1ddab62757e0628943a6c6c4ba5e7e03f71088de
Andrei Kandratsenka <andrei_kandratsenka@epam.com>
2025-04-02 22:59:48 +0300

    first hw

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
 
 function App() {
-
+  const arr = ['apple', 'banana', 'orange']
+  
   return (
-      <h1>Hello world</h1>
+    <>
+      <h1>HW1</h1>
+      {arr.map((item, index) => (<div key={index}>{item}</div>))}
+    </>
+
   )
 }
 
