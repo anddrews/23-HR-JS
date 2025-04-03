9fabe3a00e73abaca8419246393ebcf0c67c0be3
Andrei Kandratsenka <andrei_kandratsenka@epam.com>
2025-04-03 12:48:26 +0300

    HW2 DONE

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import './App.css'
-const arr = [1,  2, 3, 4, 5, 6, 7, 8, 9, 10];
+import { Button } from './components/button/Button'
 
 function App() {
 
   return (
     <>
       <h1>Hello world</h1>
-      {arr.map((item, index) => (
-        <div key={index}>
-          {item}
-        </div>
-      ))}
+      <Button/>
     </>
   )
 }
