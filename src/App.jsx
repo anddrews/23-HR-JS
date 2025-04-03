920fb77c2ebfe34539f48382b2aef418a5c95fb1
Andrey Kondratenko <anddrews@users.noreply.github.com>
2025-04-03 12:52:08 +0300

    Hw1 (#7)
    * some changes
    
    * fix comment
    
    ---------
    
    Co-authored-by: Andrei Kandratsenka <andrei_kandratsenka@epam.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
+const arr = [1,  2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function App() {
 
   return (
+    <>
       <h1>Hello world</h1>
+      {arr.map((item, index) => (
+        <div key={index}>
+          {item}
+        </div>
+      ))}
+    </>
   )
 }
 
